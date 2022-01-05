5e1959d045143275fa9f22bdada6f4ad038f9dc5
Patrice-H <https://github.com/>
2022-01-05 15:56:32 +0100

    Update links to photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
-    const link = portrait.substring(0, portrait.length - 4) + '.html';
+    //const link = portrait.substring(0, portrait.length - 4) + '.html';
+    const link = 'photographer.html?id=' + id;
     
     function getUserCardDOM() {
         const article = document.createElement('article');
         const a = document.createElement('a');
         a.setAttribute('href', link);
+        a.setAttribute('id', id);
         const img = document.createElement('img');
         img.setAttribute('src', picture);
+        img.setAttribute('alt', '');
         const h2 = document.createElement('h2');
         h2.textContent = name;
         const plocation = document.createElement('p');
