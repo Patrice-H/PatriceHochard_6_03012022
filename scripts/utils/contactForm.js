b6de61d6fe6a7eee83790adbf9dfedd8386ff4c3
Patrice-H <https://github.com/>
2022-01-18 20:11:03 +0100

    Contact modal layout

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,25 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+function displayPhotographerContact(id) {
+    // eslint-disable-next-line no-undef
+    const PHOTOGRAPHER = getPhotographerData(id);
+    const CONTACT_MODAL = document.getElementById('modal-contact-name');
+    CONTACT_MODAL.textContent = PHOTOGRAPHER.name;
 }
 
+// eslint-disable-next-line no-unused-vars
+function displayModal() {
+    const modal = document.getElementById('contact_modal');
+    const MODAL_HIDDEN = document.getElementById('modal-id');
+    // eslint-disable-next-line no-undef
+    const MODAL_ID = getPhotographerPageId();
+    MODAL_HIDDEN.setAttribute('value', MODAL_ID);
+    modal.style.display = 'block';
+
+    // eslint-disable-next-line no-undef
+    displayPhotographerContact(MODAL_ID);
+}
+
+// eslint-disable-next-line no-unused-vars
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+    const modal = document.getElementById('contact_modal');
+    modal.style.display = 'none';
 }
