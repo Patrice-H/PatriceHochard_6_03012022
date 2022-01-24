1c98e5ce5e704e8d07940872019362fd1ad6f07e
Patrice-H <https://github.com/>
2022-01-24 21:13:18 +0100

    Stylization of the contact mode and processing of submitted data

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,25 +1,51 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+
 function displayPhotographerContact(id) {
-    // eslint-disable-next-line no-undef
     const PHOTOGRAPHER = getPhotographerData(id);
     const CONTACT_MODAL = document.getElementById('modal-contact-name');
     CONTACT_MODAL.textContent = PHOTOGRAPHER.name;
 }
 
-// eslint-disable-next-line no-unused-vars
+
 function displayModal() {
     const modal = document.getElementById('contact_modal');
-    const MODAL_HIDDEN = document.getElementById('modal-id');
-    // eslint-disable-next-line no-undef
+    const MODAL_HIDDEN = document.getElementById('recipient-id');
     const MODAL_ID = getPhotographerPageId();
     MODAL_HIDDEN.setAttribute('value', MODAL_ID);
     modal.style.display = 'block';
-
-    // eslint-disable-next-line no-undef
     displayPhotographerContact(MODAL_ID);
 }
 
-// eslint-disable-next-line no-unused-vars
+
 function closeModal() {
     const modal = document.getElementById('contact_modal');
     modal.style.display = 'none';
 }
+
+
+function displayContactData() {
+    const RECIPIENT = document.getElementById('recipient-id');
+    const FIRSTNAME = document.getElementById('firstname');
+    const LASTNAME = document.getElementById('lastname');
+    const EMAIL = document.getElementById('email');
+    const MESSAGE = document.getElementById('message');
+    console.log(`Recipient id : ${RECIPIENT.value}`);
+    console.log(`First name : ${FIRSTNAME.value}`);
+    console.log(`Last name : ${LASTNAME.value}`);
+    console.log(`Email : ${EMAIL.value}`);
+    console.log(`Message : ${MESSAGE.value}`);
+
+}
+
+
+function sendModalForm() {
+    closeModal();
+    displayContactData();
+}
+
+const CONTACT_BUTTON = document.getElementById('contact-submit');
+CONTACT_BUTTON.addEventListener('click', (evt) => {
+    evt.preventDefault();
+});
