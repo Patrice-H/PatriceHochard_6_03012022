49c291ded650d0bb1786b2b2dc4662a3ad7eaa99
Patrice-H <https://github.com/>
2022-01-25 14:51:51 +0100

    Add profile article functionality in photographerFactory

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,32 +1,58 @@
-// eslint-disable-next-line no-unused-vars
-function photographerFactory(data) {
+/* eslint-disable no-unused-vars */
 
-    const {name, city, country, tagline} = data;
+function photographerFactory(data) {
     
     const pictureFileFormat = () => {
         const PATH = 'assets/photographers/';
         if (data.portrait.substring(data.portrait.length - 3) !== 'png') {
-
             return PATH + data.portrait.substring(0, data.portrait.length - 3) + 'png';
-
-        } else {
-            
+        } else {   
             return PATH + data.portrait;
         }
     };
 
-    // eslint-disable-next-line no-unused-vars
+    function displayProfile() {
+        const LINK = 'photographer.html?id=' + data.id;
+        const ARTICLE = document.createElement('article');
+        const A = document.createElement('a');
+        const IMG = document.createElement('img');
+        const H2 = document.createElement('h2');
+        const P_LOCATION = document.createElement('p');
+        const P_TAGLINE = document.createElement('p');
+        const P_PRICE = document.createElement('p');
+        
+        A.setAttribute('href', LINK);
+        A.setAttribute('title', data.name + ' gallery');
+        IMG.setAttribute('src', pictureFileFormat());
+        IMG.setAttribute('alt', '');
+        H2.textContent = data.name;
+        P_LOCATION.setAttribute('class', 'location');
+        P_LOCATION.textContent = data.city + ', ' + data.country;
+        P_TAGLINE.setAttribute('class', 'tagline');
+        P_TAGLINE.textContent = data.tagline;
+        P_PRICE.setAttribute('class', 'price');
+        P_PRICE.textContent = data.price + '€/jour';
+        A.appendChild(IMG);
+        A.appendChild(H2);
+        ARTICLE.appendChild(A);
+        ARTICLE.appendChild(P_LOCATION);
+        ARTICLE.appendChild(P_TAGLINE);
+        ARTICLE.appendChild(P_PRICE);
+
+        return ARTICLE;
+    }
+
     function displayPresentation() {
         const NAME = document.getElementById('presentation-name');
         const LOCATION = document.getElementById('presentation-location');
         const TAGLINE = document.getElementById('presentation-tagline');
         const PICTURE = document.getElementById('presentation-img');
-        NAME.textContent = `${name}`;
-        LOCATION.textContent = `${city}, ${country}`;
-        TAGLINE.textContent = `${tagline}`;
+        NAME.textContent = data.name;
+        LOCATION.textContent = data.city + ', ' + data.country;
+        TAGLINE.textContent = data.tagline;
         PICTURE.setAttribute('src', pictureFileFormat());
-        PICTURE.setAttribute('alt', `${name}`);
-        PICTURE.setAttribute('title', `${name} portrait`);
+        PICTURE.setAttribute('alt', data.name);
+        PICTURE.setAttribute('title', data.name + ' portrait');
     }
     
     return {
@@ -36,7 +62,8 @@ function photographerFactory(data) {
         country: data.country,
         tagline: data.tagline,
         price: data.price,
-        portrait: pictureFileFormat(),
+        portrait: pictureFileFormat,
+        displayProfile,
         displayPresentation
     };
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,44 +1,18 @@
-async function getPhotographers() {
-    const PHOTOGRAPHERS = fetch('./data/photographers.json')
+/* eslint-disable no-undef */
+
+function getPhotographers() {
+    return fetch('./data/photographers.json')
         .then(response => response.json())
         .then(data => data.photographers)
         .catch(err => console.log('Error : ', err))
     ;
-
-    return PHOTOGRAPHERS;
 }
 
 function displayArticle(photographers) {
     const PHOTOGRAPHERS_SECTION = document.querySelector('.photographer_section');
-
     photographers.forEach((photographer) => {
-        // eslint-disable-next-line no-undef
         const PROFILE = photographerFactory(photographer);
-        const LINK = 'photographer.html?id=' + PROFILE.id;
-        const ARTICLE = document.createElement('article');
-        const A = document.createElement('a');
-        const IMG = document.createElement('img');
-        const H2 = document.createElement('h2');
-        const P_LOCATION = document.createElement('p');
-        const P_TAGLINE = document.createElement('p');
-        const P_PRICE = document.createElement('p');
-        
-        A.setAttribute('href', LINK);
-        IMG.setAttribute('src', PROFILE.portrait);
-        IMG.setAttribute('alt', '');
-        H2.textContent = PROFILE.name;
-        P_LOCATION.setAttribute('class', 'location');
-        P_LOCATION.textContent = PROFILE.city + ', ' + PROFILE.country;
-        P_TAGLINE.setAttribute('class', 'tagline');
-        P_TAGLINE.textContent = PROFILE.tagline;
-        P_PRICE.setAttribute('class', 'price');
-        P_PRICE.textContent = PROFILE.price + '€/jour';
-        A.appendChild(IMG);
-        A.appendChild(H2);
-        ARTICLE.appendChild(A);
-        ARTICLE.appendChild(P_LOCATION);
-        ARTICLE.appendChild(P_TAGLINE);
-        ARTICLE.appendChild(P_PRICE);
+        const ARTICLE = PROFILE.displayProfile();
         PHOTOGRAPHERS_SECTION.appendChild(ARTICLE);
     });
 }
